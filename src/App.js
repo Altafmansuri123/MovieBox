78678448a872facddc70ff00bb2d55b05d4a62b7
Altafmansuri123 <altafmansuri780@gmail.com>
2022-01-01 18:21:11 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Accordian from './components/Accordian';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Hello World</h1>
+            <Accordian />
+        </div>
+    );
+}
+
+export default App;
