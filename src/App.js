34fae334431231e896fd78d15afa3a7c0b93e6fa
Altafmansuri123 <altafmansuri780@gmail.com>
2022-01-02 09:19:22 +0530

    Created Routes successfully

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import SimpleBottomNavigation from './components/MainNav';
 
-
 function App() {
-    return (
-        <>
-            <Header />
-            <div className="app">
-                <SimpleBottomNavigation />
-
-            </div>
-        </>
-
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <div className='app'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<Trending />} />
+            <Route path='/movies' element={<Movies />} />
+            <Route path='/series' element={<Series />} />
+            <Route path='/search' element={<Search />} />
+          </Routes>
+        </Container>
+      </div>
+      <SimpleBottomNavigation />
+    </BrowserRouter>
+  );
 }
 
 export default App;
